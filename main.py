bb404169e58286c6c806502db30cacfa542078c7
Sarosh Malak <malaksarosh425@gmail.com>
2026-05-15 23:26:59 +0530

    fixed routes

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Home Route
+@app.get("/")
+def home():
+    return {"message": "Server Running"}
+
+# Test Route
 @app.get("/test")
 def test():
     return {"message": "Backend Connected"}
